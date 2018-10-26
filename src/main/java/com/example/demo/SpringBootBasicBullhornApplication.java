f9be35191259e8bd08917790a9f3b14b2c669a2f
carina <cncnewton37@gmail.com>
2018-10-26 10:53:50 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBootBasicBullhornApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicBullhornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicBullhornApplication.class, args);
+	}
+}
